4fcbcb358f3449546e4c47d750a7fbc080d4858c
open cv <112242723+Yaswanth111494@users.noreply.github.com>
2023-05-15 10:23:21 +0530

    Update open cv 180.py

--- open cv 180.py
+++ open cv 180.py
@@ -1,18 +1,13 @@
 # rotattion 180
 import cv2
 
-
 path = "C:/Users/yaswa/Pictures/Screenshots/Screenshot (30).png"
-
 
 src = cv2.imread(path)
 
-
 window_name = 'Image'
 
-
 image = cv2.rotate(src, cv2.ROTATE_180)
-
 
 cv2.imshow(window_name, image)
 cv2.waitKey(0)
